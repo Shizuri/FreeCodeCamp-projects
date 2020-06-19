53dec7fe6834d614e04f84133bf28c4ada2348a2
Zdravko Mavkov <mavkov@hotmail.com>
2020-06-19 11:47:52 +0200

    Went back and fixed Techincal-documentation

--- Technical-documentation-page/nav.js
+++ Technical-documentation-page/nav.js
@@ -1,18 +1,18 @@
-const show = () => {
-    const navElements = document.getElementsByClassName('nav-link-mobile')
+const toggleNavMenu = () => {
     // Get all the navigation links for mobile
+    const navElements = document.getElementsByClassName('nav-link-mobile')
 
     for (let i = 0; i < navElements.length; i++) {
-        const navElementsStyle = getComputedStyle(navElements[i])
         // Get the computed values for the style. This is necessary when the style is in a .css file
-        navElementsStyle.display === 'none' ? navElements[i].style.display = 'block' : navElements[i].style.display = 'none'
+        const navElementsStyle = getComputedStyle(navElements[i])
         // Toggle the diplay value
+        navElementsStyle.display === 'none' ? navElements[i].style.display = 'block' : navElements[i].style.display = 'none'
     }
 
-    const mainNavElement = document.getElementById('navigation-popup')
     // Get the navigation element
-    const mainNavElementStyle = getComputedStyle(mainNavElement)
+    const mainNavElement = document.getElementById('navigation-popup')
     // Get its computed style
-    mainNavElementStyle.color === 'rgb(0, 0, 0)' ? mainNavElement.style.color = 'blueviolet' : mainNavElement.style.color = 'black'
+    const mainNavElementStyle = getComputedStyle(mainNavElement)
     // Toggle its color to show that it's enabled or disabled
+    mainNavElementStyle.color === 'rgb(0, 0, 0)' ? mainNavElement.style.color = 'blueviolet' : mainNavElement.style.color = 'black'
 }
