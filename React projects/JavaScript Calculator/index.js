b2567453e69bb6a96309a2f108fcf16f82f1816e
Zdravko Mavkov <mavkov@hotmail.com>
2020-11-09 12:49:14 +0100

    Started working on JavaScript Calculator

---
+++ React projects/JavaScript Calculator/index.js
@@ -0,0 +1,20 @@
+const { useState, useEffect, useRef } = React;
+
+const App = (props) => {
+    return (
+        <div className="App">
+            test
+        </div>
+    );
+};
+
+const Pad = (props) => {
+    return (
+        <div className="drum-pad">
+        </div>
+    );
+};
+
+// ======================
+
+ReactDOM.render(<App />, document.getElementById("root"));
